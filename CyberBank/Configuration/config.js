47a029a7cd8c950a1b326fa55fda1df51694b3d9
mayibee <42274714+mayibee@users.noreply.github.com>
2019-01-08 20:06:49 -0500

    Update config.js

--- CyberBank/Configuration/config.js
+++ CyberBank/Configuration/config.js
@@ -1,11 +1,11 @@
 exports.config = {
-	specs: ['../Tests/demo.spec.js'],
+// 	specs: ['../Tests/demo.spec.js'],
 
-// 	suites:{
+	suites:{
 
-// 		smoke:['../Tests/demo.spec.js'],
-// 		regression:['../Tests/*.spec.js']
-// 	},
+		smoke:['../Tests/demo.spec.js'],
+		regression:['../Tests/*.spec.js']
+	},
  
 	sauceUser: process.env.SAUCE_USERNAME,
 	sauceKey: process.env.SAUCE_ACCESS_KEY,
@@ -13,14 +13,14 @@ exports.config = {
 	// restartBrowserBetweenTests: true,
 
     multiCapabilities: [
-		{
-        browserName: 'firefox',
-        version: '61.0',
-        platform: 'macOS 10.14',
-        name: "firefox-tests",
-        shardTestFiles: true,
-        maxInstances: 25
-	}, 
+// 		{
+//         browserName: 'firefox',
+//         version: '61.0',
+//         platform: 'macOS 10.14',
+//         name: "firefox-tests",
+//         shardTestFiles: true,
+//         maxInstances: 25
+// 	}, 
 	{
         browserName: 'chrome',
         version: '70.0',
